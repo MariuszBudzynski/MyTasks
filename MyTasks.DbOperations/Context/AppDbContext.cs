365d0fa81d57b50c4f2eadf909ed3a6d91f6fc2d
Hood <mariusz.budzynski87@gmail.com>
2025-08-13 08:17:47 +0200

    Added dbContext to DI and changed the path

--- MyTasks.DbOperations/Context/AppDbContext.cs
+++ MyTasks.DbOperations/Context/AppDbContext.cs
@@ -5,17 +5,7 @@ namespace MyTasks.DbOperations.Context
 {
     public class AppDbContext : DbContext
     {
-
-        private readonly string _dbPath;
-
-        public AppDbContext()
-        {
-            var projectFolder = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "..", "MyDataLibrary"));
-            _dbPath = Path.Combine(projectFolder, "Database.db");
-        }
-
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-           => optionsBuilder.UseSqlite($"Data Source={_dbPath}");
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
--- MyTasks/Services/ServisRegistration.cs
+++ MyTasks/Services/ServisRegistration.cs
@@ -1,11 +1,21 @@
-﻿namespace MyTasks.Services
+﻿using Microsoft.EntityFrameworkCore;
+using MyTasks.DbOperations.Context;
+
+namespace MyTasks.Services
 {
     public static class ServisRegistration
     {
         public static void Register(this WebApplicationBuilder builder)
         {
+            var relativePath = builder.Configuration.GetConnectionString("DefaultConnection");
+            var projectFolder = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", ".."));
+            var dbPath = Path.Combine(projectFolder, relativePath);
+
             // Add services to the container.
             builder.Services.AddRazorPages();
+
+            builder.Services.AddDbContext<AppDbContext>(options =>
+               options.UseSqlite($"Data Source={dbPath}"));
         }
     }
 }

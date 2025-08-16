3d4734cefacc83f88d0db5d6d93ca272430e6e9b
Hood <mariusz.budzynski87@gmail.com>
2025-08-16 08:39:10 +0200

    changed to AJAX support

--- MyTasks/ClientApp/src/pages/login/Login.jsx
+++ MyTasks/ClientApp/src/pages/login/Login.jsx
@@ -2,24 +2,31 @@ import React from "react";
 
 export default function Login() {
   return (
-    <form method="post" className="space-y-4">
-      <div>
-        <label>User Name:</label>
-        <input type="text" name="Username" className="border p-2 w-full" />
+    <div className="max-w-sm mx-auto mt-10 space-y-4">
+      <div className="flex items-center">
+        <label className="mr-4 w-24">User Name:</label>
+        <input type="text" name="Username" className="border p-2 flex-1" />
       </div>
 
-      <div>
-        <label>Password:</label>
+      <div className="flex items-center">
+        <label className="mr-4 w-24">Password:</label>
         <input
           type="password"
           name="PasswordHash"
-          className="border p-2 w-full"
+          className="border p-2 flex-1"
         />
       </div>
 
-      <button type="submit" className="bg-blue-600 text-white p-2 rounded">
+      <button
+        type="button"
+        className="bg-blue-600 text-white p-2 w-full mt-4"
+        onClick={() => {
+          // AJAX goes here
+          console.log("Login clicked");
+        }}
+      >
         Login
       </button>
-    </form>
+    </div>
   );
 }

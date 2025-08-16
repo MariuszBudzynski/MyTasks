647fd523a5fb3673b76351f98bc1da3f0a6b5aca
Hood <mariusz.budzynski87@gmail.com>
2025-08-16 15:12:49 +0200

    Config change

--- MyTasks/ClientApp/src/pages/login/Login.jsx
+++ MyTasks/ClientApp/src/pages/login/Login.jsx
@@ -2,24 +2,29 @@ import React from "react";
 
 export default function Login() {
   return (
-    <div className="max-w-sm mx-auto mt-10 space-y-4">
-      <div className="flex items-center">
-        <label className="mr-4 w-24">User Name:</label>
-        <input type="text" name="Username" className="border p-2 flex-1" />
+    <div
+      id="login-container"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "15px",
+        width: "300px",
+        margin: "50px auto",
+      }}
+    >
+      <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+        <label>User Name:</label>
+        <input type="text" name="Username" />
       </div>
 
-      <div className="flex items-center">
-        <label className="mr-4 w-24">Password:</label>
-        <input
-          type="password"
-          name="PasswordHash"
-          className="border p-2 flex-1"
-        />
+      <div style={{ display: "flex", alignItems: "center", gap: "10px" }}>
+        <label>Password:</label>
+        <input type="password" name="PasswordHash" />
       </div>
 
       <button
         type="button"
-        className="bg-blue-600 text-white p-2 w-full mt-4"
+        style={{ padding: "8px 12px" }}
         onClick={() => {
           // AJAX goes here
           console.log("Login clicked");

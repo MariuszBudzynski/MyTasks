a1ab35b6c6992d3e260cec6bd5ac0555820f165e
Hood <mariusz.budzynski87@gmail.com>
2025-08-19 09:42:52 +0200

    refactor

--- MyTasks/Services/LoginValidator.cs
+++ MyTasks/Services/LoginValidator.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.IdentityModel.Tokens;
 using MyTasks.Models.Models;
 using MyTasks.Repositories.Interfaces.ILoginRepository;

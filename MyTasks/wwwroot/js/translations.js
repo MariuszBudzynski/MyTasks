5228e65b788b7e9c04ef8ef47574b14ddcf4ba45
Hood <mariusz.budzynski87@gmail.com>
2025-08-30 08:40:41 +0200

    Added script to translate buttons on layout for simplicity

---
+++ MyTasks/wwwroot/js/translations.js
@@ -0,0 +1,19 @@
+﻿//as the translation are small, no need to prep.the full resex architecture
+
+const translations = {
+    LogoutButton: { en: "Logout", pl: "Wyloguj" },
+};
+
+function applyTranslations() {
+    const culture = navigator.language.startsWith('pl') ? 'pl' : 'en';
+    document.querySelectorAll("[data-i18n]").forEach(el => {
+        const key = el.getAttribute("data-i18n");
+
+        if (translations[key]) {
+            el.textContent = translations[key][culture];
+        }
+    });
+}
+
+//we run the script after the full page is loaded
+document.addEventListener("DOMContentLoaded", applyTranslations);

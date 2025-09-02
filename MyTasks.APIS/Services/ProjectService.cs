dd17ef9f6a12960987a159ebff130b68c2ca88b0
Hood <mariusz.budzynski87@gmail.com>
2025-09-02 14:45:09 +0200

    refactor

--- MyTasks.APIS/Services/ProjectService.cs
+++ MyTasks.APIS/Services/ProjectService.cs
@@ -9,27 +9,19 @@ namespace MyTasks.API.Services
     public class ProjectService : IProjectService
     {
         private readonly IProjecRepository _projectRepository;
-        private readonly IUserOperationsRepository _userRepository;
-
         public ProjectService(IProjecRepository projectRepository, IUserOperationsRepository userRepository)
         {
             _projectRepository = projectRepository;
-            _userRepository = userRepository;
         }
 
         public async Task<ProjectResponseDto> CreateProjectAsync(CreateProjectDto data, Guid ownerId)
         {
-            var user = await _userRepository.GetUserByIdAsync(ownerId);
-            if (user == null)
-                throw new InvalidOperationException("User does not exist.");
-
             var project = new ProjectModel
             {
                 Id = Guid.NewGuid(),
                 Name = data.Name,
                 Description = data.Description,
                 OwnerId = ownerId,
-                Owner = user
             };
 
             await _projectRepository.AddProject(project);

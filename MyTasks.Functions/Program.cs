0886101a080509f3f9b8c20649066d77f78f3bc3
Hood <mariusz.budzynski87@gmail.com>
2025-09-30 10:25:02 +0200

    changed Program CS

--- MyTasks.Functions/Program.cs
+++ MyTasks.Functions/Program.cs
@@ -11,18 +11,16 @@ using MyTasks.Repositories.Interfaces.IProjecRepository;
 using MyTasks.Repositories.Repositories.ProjecRepository;
 
 var builder = FunctionsApplication.CreateBuilder(args);
+
 var relativePath = builder.Configuration.GetConnectionString("DefaultConnection");
 var projectFolder = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", ".."));
 var dbPath = Path.Combine(projectFolder, relativePath);
-
-builder.ConfigureFunctionsWebApplication();
 
 builder.Services
     .AddApplicationInsightsTelemetryWorkerService()
     .ConfigureFunctionsApplicationInsights()
     .AddDbContext<AppDbContext>(options =>
-     options.UseSqlite($"Data Source={dbPath}"))
-
+        options.UseSqlite($"Data Source={dbPath}"))
     .AddScoped<IProjecRepository, ProjecRepository>()
     .AddScoped<IProjectOperationsRepository, ProjectOperationsRepository>()
     .AddScoped(typeof(IDbRepository<>), typeof(DbRepository<>));
